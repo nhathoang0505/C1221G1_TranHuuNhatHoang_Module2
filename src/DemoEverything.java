dbea6e5a2391283be0f9b3d53dd0cac2664681d6
nhathoang0505 <nhathoang0505@gmail.com>
2022-02-21 00:35:11 +0700

    nop bai thuc hanh

--- src/DemoEverything.java
+++ src/DemoEverything.java
@@ -1,11 +1,5 @@
 public class DemoEverything {
     public static void main(String[] args) {
-        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9},
 
-                b = new int[3];
-
-        a[9] = b[0];
-
-        System.out.println(a[9]);
     }
 }
