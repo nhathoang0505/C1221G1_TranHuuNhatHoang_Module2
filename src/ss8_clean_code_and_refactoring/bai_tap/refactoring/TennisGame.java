e6f619c2039bd8f8eac8f294c3e6bf46183bb604
nhathoang0505 <nhathoang0505@gmail.com>
2022-02-26 12:45:06 +0700

    update

--- src/ss8_clean_code_and_refactoring/bai_tap/refactoring/TennisGame.java
+++ src/ss8_clean_code_and_refactoring/bai_tap/refactoring/TennisGame.java
@@ -1,73 +1,81 @@
 package ss8_clean_code_and_refactoring.bai_tap.refactoring;
 
+
 public class TennisGame {
+    public static final int LOVE = 0;
+    public static final int FIFTEEN = 1;
+    public static final int THIRTY = 2;
+    public static final int FORTY = 3;
+    public static final int FiNALPOINT = 4;
 
-
-    public static String getScore(String player1Name, String player2Name, int player1Score, int player2Score) {
-        StringBuilder score = new StringBuilder();
-        int tempScore = 0;
-        if (isScoreEquals(player1Score, player2Score)) {
-            switch (player1Score) {
-                case 0:
-                    score = new StringBuilder("Love-All");
-                    break;
-                case 1:
-                    score = new StringBuilder("Fifteen-All");
-                    break;
-                case 2:
-                    score = new StringBuilder("Thirty-All");
-                    break;
-                case 3:
-                    score = new StringBuilder("Forty-All");
-                    break;
-                default:
-                    score = new StringBuilder("Deuce");
-                    break;
-
-            }
-        } else if (isDeuce(player1Score, player2Score)) {
-            score = checkScore(player1Score, player2Score);
+    public static String getScore(String player1Name, String player2Name, int scorePlayer1, int scorePlayer2) {
+        String score = "";
+        int tempScore = LOVE;
+        if (scorePlayer1 == scorePlayer2) {
+            score = displayDrawScore(scorePlayer1);
+        } else if (scorePlayer1 >= FiNALPOINT || scorePlayer2 >= FiNALPOINT) {
+            score = displayWinner(scorePlayer1, scorePlayer2);
         } else {
-            for (int i = 1; i < 3; i++) {
-                if (isScoreEquals(i, 1)) tempScore = player1Score;
-                else {
-                    score.append("-");
-                    tempScore = player2Score;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score.append("Love");
-                        break;
-                    case 1:
-                        score.append("Fifteen");
-                        break;
-                    case 2:
-                        score.append("Thirty");
-                        break;
-                    case 3:
-                        score.append("Forty");
-                        break;
-                }
-            }
+            score = displayScore(scorePlayer1, scorePlayer2, score);
         }
-        return score.toString();
-    }
-
-    private static StringBuilder checkScore(int player1Score, int player2Score) {
-        StringBuilder score;
-        int minusResult = player1Score - player2Score;
-        if (isScoreEquals(minusResult, 1)) score = new StringBuilder("Advantage player1");
-        else if (isScoreEquals(minusResult, -1)) score = new StringBuilder("Advantage player2");
-        else if (minusResult >= 2) score = new StringBuilder("Win for player1");
-        else score = new StringBuilder("Win for player2");
         return score;
     }
 
-    private static boolean isDeuce(int player1Score, int player2Score) {
-        return player1Score >= 4 || player2Score >= 4;
+    private static String displayDrawScore(int scorePlayer1) {
+        String score;
+        switch (scorePlayer1) {
+            case LOVE:
+                score = "Love-All";
+                break;
+            case FIFTEEN:
+                score = "Fifteen-All";
+                break;
+            case THIRTY:
+                score = "Thirty-All";
+                break;
+            case FORTY:
+                score = "Forty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+        }
+        return score;
     }
 
-    private static boolean isScoreEquals(int player1Score, int player2Score) {
-        return player1Score == player2Score;
+    private static String displayScore(int scorePlayer1, int scorePlayer2, String score) {
+        int tempScore;
+        for (int i = FIFTEEN; i < FORTY; i++) {
+            if (i == FIFTEEN) tempScore = scorePlayer1;
+            else {
+                score += "-";
+                tempScore = scorePlayer2;
+            }
+            switch (tempScore) {
+                case LOVE:
+                    score += "Love";
+                    break;
+                case FIFTEEN:
+                    score += "Fifteen";
+                    break;
+                case THIRTY:
+                    score += "Thirty";
+                    break;
+                case FORTY:
+                    score += "Forty";
+                    break;
+            }
+        }
+        return score;
+    }
+
+    private static String displayWinner(int scorePlayer1, int scorePlayer2) {
+        String score;
+        int minusResult = scorePlayer1 - scorePlayer2;
+        if (minusResult == 1) score = "Advantage player1";
+        else if (minusResult == -1) score = "Advantage player2";
+        else if (minusResult >= 2) score = "Win for player1";
+        else score = "Win for player2";
+        return score;
     }
 }
